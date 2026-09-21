b8f57623650e7eea797e86a099bbcea25fc25e31
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-09-21 15:35:44 +0500

    Initial Commit

---
+++ client/src/components/Featuredsection.jsx
@@ -0,0 +1,50 @@
+import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { dummyShowsData } from "../assets/assets";
+import BlurSection from "./Blurcircle";
+import MovieCard from "./MovieCard";
+
+const FeaturedSection = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="px-6 md:px-16 lg:px-36">
+      <div className="relative flex items-center justify-between pt-20 pb-10">
+        <BlurSection top="0" right="-80px" />
+        <p className="text-gray-300 font-medium text-lg md:text-xl tracking-wide">
+          Now Showing
+        </p>
+        <button
+          onClick={() => {
+            navigate("/movies");
+            window.scrollTo(0, 0);
+          }}
+          className="group flex items-center gap-2 text-sm text-gray-300 hover:text-white transition cursor-pointer"
+        >
+          <span>View All</span>
+          <ArrowRight className="group-hover:translate-x-1 transition-transform w-4 h-4" />
+        </button>
+      </div>
+
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show._id} movie={show} />
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            window.scrollTo(0, 0);
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+        >
+          Show More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedSection;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import FeaturedSection from "../components/Featuredsection";
+import HeroSection from "../components/Herosection";
+import TrailerSection from "../components/TrailerSection";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturedSection />
+      <TrailerSection />
+    </>
+  );
+};
+
+export default Home;
