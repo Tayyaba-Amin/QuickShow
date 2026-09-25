21b50cca76176d5529c324d85b53ae2aa94da231
Tayyaba Amin <tayyabaamin780@gmail.com>
2026-09-25 21:52:01 +0500

    fix issue

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AdminNavbar from "../../components/admin/AdminNavBar";
+import AdminNavbar from "../../components/admin/AdminNavbar";
 import AdminSideBar from "../../components/admin/AdminSideBar";
 import { Outlet } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
